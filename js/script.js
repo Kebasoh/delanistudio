a6b47104c861397520408c41e3f9d0d732e82870
Kebasoh <ongatikebaso@gmail.com>
2019-11-01 13:46:46 +0300

    added the toggle id

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('#toggle').click(function () {
+        $('#p1').show();
+    });
+    $('#toggle').click(function () {
+        $('#p1').hide();
+    });
+
+    $('#toggle').click(function () {
+        $('#p1').show();
+    });
+    $('#p1').click(function () {
+        $('#p1').hide();
+    }); 
+        
+    });
+    $('#p2').click(function () {
+
+
+});
